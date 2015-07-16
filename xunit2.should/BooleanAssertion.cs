c7d255ab56b0259a174f1a75627989cd91c1ca46
Ruslan <ruslan.albu@hotmail.com>
2015-07-16 16:17:11 +0100

    xunit packages added

--- xunit2.should/BooleanAssertion.cs
+++ xunit2.should/BooleanAssertion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Xunit2.Should
 {
@@ -10,7 +11,7 @@ namespace Xunit2.Should
     {
         public static void ShouldBeTrue(this bool val)
         {
-            
+            Assert.True(val);
         }
     }
 }
