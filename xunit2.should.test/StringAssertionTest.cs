4b2066bbafcf1c31f8e31770528485e94a650938
Ruslan <ruslan.albu@hotmail.com>
2015-07-29 16:52:25 +0100

    String assertions added

---
+++ xunit2.should.test/StringAssertionTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit2.Should;
+
+namespace xunit2.should.test
+{
+    public class StringAssertionTest
+    {
+        [Fact]
+        public void ShouldContain()
+        {
+            "xunit".ShouldContain("unit");
+        }
+
+        [Fact]
+        public void ShouldContainWithComparison()
+        {
+            "xunit".ShouldContain("UNIT", StringComparison.CurrentCultureIgnoreCase); }
+
+        [Fact]
+        public void ShouldNotContain()
+        {
+            "xunit".ShouldNotContain("nunit");
+        }
+
+        [Fact]
+        public void ShouldNotContainWithComparison()
+        {
+           "xunit".ShouldNotContain("nunit", StringComparison.CurrentCultureIgnoreCase); 
+        }
+
+        [Fact]
+        public void ShouldStartWith()
+        {
+            "xunitTest".ShouldStartWith("xunit");
+        }
+
+        [Fact]
+        public void ShouldStartWithComparison()
+        {
+            "xunitTest".ShouldStartWith("XUNIT", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void ShouldEndWith()
+        {
+            "testxunit".ShouldEndWith("xunit");
+        }
+        [Fact]
+        public void ShouldEndWithComparision()
+        {
+            "testxunit".ShouldEndWith("xunit", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void ShouldMatch()
+        {
+            "xunit".ShouldMatch(@"\w");
+        }
+
+        [Fact]
+        public void ShouldMatchRegEx()
+        {
+            "xunit".ShouldMatch(new Regex(@"\w"));
+        }
+
+        [Fact]
+        public void ShouldNotMatch()
+        {
+            "xunit".ShouldNotMatch(@"\d");
+        }
+
+        [Fact]
+        public void ShouldNotMatchRegEx()
+        {
+            "xunit".ShouldNotMatch(new Regex(@"\d"));
+        }
+
+        [Fact]
+        public void ShouldBeEqual()
+        {
+            "xunit".ShouldBeEqual("xunit");
+        }
+
+        [Fact]
+        public void ShouldBeEqualIgnoreCase()
+        {
+            "xunit".ShouldBeEqual("xUnit ", ignoreCase: true);
+        }
+
+    }
+}
---
+++ xunit2.should/StringAssertion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Xunit2.Should
+{
+    public static class StringAssertion
+    {
+        public static void ShouldContain(this string actual, string substring)
+        {
+            Assert.Contains(substring, actual);
+        }
+
+        public static void ShouldContain(this string actual, string substring, StringComparison comparison)
+        {
+            Assert.Contains(substring, actual, comparison);
+        }
+
+        public static void ShouldNotContain(this string actual, string substring)
+        {
+            Assert.DoesNotContain(substring, actual);
+        }
+        public static void ShouldNotContain(this string actual, string substring, StringComparison comparison)
+        {
+            Assert.DoesNotContain(substring, actual, comparison);
+        }
+
+        public static void ShouldStartWith(this string actual, string expectedStartWith)
+        {
+            Assert.StartsWith(expectedStartWith, actual);
+        }
+        
+        public static void ShouldStartWith(this string actual, string expectedStartWith, StringComparison comparison)
+        {
+            Assert.StartsWith(expectedStartWith, actual, comparison);
+        }
+
+        public static void ShouldEndWith(this string actual, string expectedEndWith)
+        {
+           Assert.EndsWith(expectedEndWith, actual); 
+        }
+
+        public static void ShouldEndWith(this string actual, string expectedEndWith, StringComparison comparison)
+        {
+           Assert.EndsWith(expectedEndWith, actual, comparison); 
+        }
+
+        public static void ShouldMatch(this string actual, string regex)
+        {
+            Assert.Matches(regex, actual);
+        }
+
+        public static void ShouldMatch(this string actual, Regex regex)
+        {
+            Assert.Matches(regex, actual);
+        }
+
+        public static void ShouldNotMatch(this string actual, string regex)
+        {
+            Assert.DoesNotMatch(regex, actual);
+        }
+
+        public static void ShouldNotMatch(this string actual, Regex regex)
+        {
+            Assert.DoesNotMatch(regex, actual);
+        }
+
+        public static void ShouldBeEqual(this string expected, string actual, bool ignoreCase = false,
+            bool ignoreLineEndingDifferences = false, bool ignoreWhiteSpaceDifferences = false)
+        {
+            Assert.Equal(expected, actual, ignoreCase, ignoreLineEndingDifferences, ignoreWhiteSpaceDifferences);
+        }
+ 
+    }
+}
