d9865a4a86b5da894718231713d6f17039d702c1
Ruslan Albu <ruslanalbu@page.com>
2016-03-17 09:39:27 +0000

    equality assertion started

---
+++ xunit2.should.test/EqualityAssertionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit2.Should;
+
+namespace xunit2.should.test
+{
+    public class EqualityAssertionTest
+    {
+        [Fact]
+        public void IntsShouldBeEqual()
+        {
+            1.ShouldBe(1);
+        }
+
+        [Fact]
+        public void StringsShouldBeEqual()
+        {
+            "xunit".ShouldBe("xunit");
+        }
+    }
+}
