7a60dce7eba32363922f278aaee5cade3491e955
Ruslan Albu <ruslanalbu@page.com>
2016-05-09 17:06:30 +0100

    greater and less

--- xunit2.should.test/RangeAssertionTest.cs
+++ xunit2.should.test/RangeAssertionTest.cs
@@ -13,21 +13,94 @@ namespace xunit2.should.test
         }
 
         [Fact]
-        public void ShouldNotBeInRange()
-        {
-            1.ShouldNotBeInRange(2, 4);
-        }
-
-        [Fact]
         public void ShouldBeInRangeWithComparer()
         {
             4.ShouldBeInRange(1, 5, new IntComparer());
         }
 
         [Fact]
+        public void ShouldNotBeInRange()
+        {
+            1.ShouldNotBeInRange(2, 4);
+        }
+
+        [Fact]
         public void ShouldNotBeInRangeWithComparer()
         {
             6.ShouldNotBeInRange(1, 5, new IntComparer());
+        }
+
+        [Fact]
+        public void ShouldBeGreater()
+        {
+            9.ShouldBeGreaterThan(8);
+        }
+
+        [Fact]
+        public void ShouldBeGreaterWithComparer()
+        {
+            7.ShouldBeGreaterThan(4, new IntComparer());
+        }
+
+        [Fact]
+        public void ShouldBeGreaterOrEqual_Equal()
+        {
+            1.ShouldBeGreaterOrEqualTo(1);
+        }
+
+        [Fact]
+        public void ShouldBeGreaterOrEqualWithComparer_Equal()
+        {
+            3.ShouldBeGreaterOrEqualTo(3, new IntComparer());
+        }
+
+        [Fact]
+        public void ShouldBeGreaterOrEqual_Greater()
+        {
+            2.ShouldBeGreaterOrEqualTo(1);
+        }
+
+        [Fact]
+        public void ShouldBeGreaterOrEqualWithComparer_Greater()
+        {
+            8.ShouldBeGreaterOrEqualTo(3, new IntComparer());
+        }
+
+
+        [Fact]
+        public void ShouldBeLess()
+        {
+            2.ShouldBeLessThan(5);
+        }
+
+        [Fact]
+        public void ShouldBeLessWithComparer()
+        {
+            3.ShouldBeLessThan(4, new IntComparer());
+        }
+
+        [Fact]
+        public void ShouldBeLessOrEqual_Equal()
+        {
+            3.ShouldBeLessOrEqualTo(3);
+        }
+
+        [Fact]
+        public void ShouldBeLessOrEqualWithComparer_Equal()
+        {
+            4.ShouldBeLessOrEqualTo(4, new IntComparer());
+        }
+
+        [Fact]
+        public void ShouldBeLessOrEqual_Less()
+        {
+            3.ShouldBeLessOrEqualTo(4);
+        }
+
+        [Fact]
+        public void ShouldBeLessOrEqualWithComparer_Less()
+        {
+            4.ShouldBeLessOrEqualTo(7, new IntComparer());
         }
     }
 
--- xunit2.should/RangeAssertion.cs
+++ xunit2.should/RangeAssertion.cs
@@ -25,5 +25,46 @@ namespace Xunit2.Should
         {
             Assert.NotInRange(actual, low, high, comparer);
         }
+
+        public static void ShouldBeGreaterThan<T>(this T actual, T high) where T: IComparable
+        {
+            Assert.True(actual.CompareTo(high) > 0);
+
+        }
+        public static void ShouldBeGreaterThan<T>(this T actual, T high, IComparer<T> comparer) where T: IComparable
+        {
+            Assert.True(comparer.Compare(actual, high) > 0);
+
+        }
+        public static void ShouldBeGreaterOrEqualTo<T>(this T actual, T high) where T: IComparable
+        {
+            Assert.True(actual.CompareTo(high) >= 0);
+
+        }
+        public static void ShouldBeGreaterOrEqualTo<T>(this T actual, T high, IComparer<T> comparer) where T: IComparable
+        {
+            Assert.True(comparer.Compare(actual, high) >= 0);
+
+        }
+        public static void ShouldBeLessThan<T>(this T actual, T high) where T: IComparable
+        {
+            Assert.True(actual.CompareTo(high) < 0);
+
+        }
+        public static void ShouldBeLessThan<T>(this T actual, T high, IComparer<T> comparer) where T: IComparable
+        {
+            Assert.True(comparer.Compare(actual, high) < 0);
+
+        }
+        public static void ShouldBeLessOrEqualTo<T>(this T actual, T high) where T: IComparable
+        {
+            Assert.True(actual.CompareTo(high) <= 0);
+
+        }
+        public static void ShouldBeLessOrEqualTo<T>(this T actual, T high, IComparer<T> comparer) where T: IComparable
+        {
+            Assert.True(comparer.Compare(actual, high) <= 0);
+
+        }
     }
 }
